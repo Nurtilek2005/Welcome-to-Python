e12e3048e5937655172f1a26949269e6372efb65
Nurtilek <nurtileks2005@gmail.com>
2023-03-12 12:56:59 +0600

    added function prompt_number

--- homeWork_1/task_2/main.py
+++ homeWork_1/task_2/main.py
@@ -7,3 +7,19 @@
 100 -> 1 (1 + 0 + 0)
 """
 
+def prompt_number(message: str, attempts: int = 3) -> int:
+    while attempts > 0:
+        print(message)
+        line: str = input(">>> ")
+        if not line.isnumeric():
+            print("Your input is not number!")
+            attempts -= 1
+            continue
+        return int(line)
+    print("Yours attempts is out!")
+    return -1
+
+
+number: int = prompt_number("Enter number")
+
+
