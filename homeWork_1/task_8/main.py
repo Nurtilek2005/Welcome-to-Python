2cb7bf58e61bd49913beb2036a253d895ea687f8
Nurtilek <nurtileks2005@gmail.com>
2023-03-14 21:15:29 +0600

    finished homeWork_1's task_8

--- homeWork_1/task_8/main.py
+++ homeWork_1/task_8/main.py
@@ -9,3 +9,31 @@
 3 2 1 -> no
 """
 
+def can_break_chocolate(n, m, k):
+    if k == n * m:
+        return True
+    if k % n == 0 or k % m == 0:
+        return True
+    return False
+
+
+def prompt_number(message: str, attempts: int = 3) -> int:
+    while attempts > 0:
+        print(message)
+        line: str = input(">>> ")
+        if not line.isnumeric():
+            print("Your input is not number!")
+            attempts -= 1
+            continue
+        return int(line)
+    print("Yours attempts is out!")
+    exit(1)
+
+
+num_n: int = prompt_number("Введите число N")
+num_m: int = prompt_number("Введите число M")
+num_k: int = prompt_number("Сколько разрезов")
+
+can_break = can_break_chocolate(num_n, num_m, num_k)
+if can_break: print("yes")
+else: print("no")
