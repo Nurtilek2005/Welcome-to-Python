45f50bd6093f8a3aa772c9ad0ae310743fbd4b7d
Nurtilek2005 <nurtileks2005@gmail.com>
2023-03-30 12:27:32 +0600

    finished homeWork_5's task_28

--- homeWork_5/task_28/main.py
+++ homeWork_5/task_28/main.py
@@ -10,3 +10,41 @@
     4
 """
 
+
+def prompt_number(message: str = "", pos_only: bool = False) -> int:
+    line: str = ""
+    number: int = 0
+    while line.strip() == "":
+        if not message.strip() == "":
+            print(message)
+        try:
+            line = input(">>> ").strip()
+        except KeyboardInterrupt:
+            break
+        if line == "":
+            print("Вы ничего не ввели!")
+            line = ""
+            continue
+        try:
+            number = int(line)
+        except ValueError:
+            print("Ваше введенное значение не является числом!")
+            line = ""
+            continue
+        if pos_only and number < 0:
+            print("Ваше введенное число неотрицательное!")
+            line = ""
+            continue
+    return number
+
+
+def sum_numbers(number_a: int, number_b: int):
+    if number_b == 0:
+        return number_a
+    return sum_numbers(number_a + 1, number_b - 1)
+
+
+number_1: int = prompt_number("Введите число A")
+number_2: int = prompt_number("Введите число B")
+result: int = sum_numbers(number_1, number_2)
+print(f"A = {number_1}; B = {number_2} -> {result}")
